359d99d5f5b40e7e23e525a87ed6b5d1c1c803fe
Nikita <superduperproger@gmail.com>
2020-02-15 11:04:34 +0300

    Added html templates v0.1.0

--- main.go
+++ main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	Config "EcderGo/app/config"
-	indexHandler "EcderGo/app/handls"
 	"fmt"
-	"log"
+	"html/template"
 	"net/http"
 )
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("temp/index.html", "temp/header.html", "temp/footer.html")
+
+	if err != nil {
+		_, _ = fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
+
 func main() {
-	http.HandleFunc("/", indexHandler.IndexHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", IndexHandler)
 
 	conf := Config.InitConfig()
 
 	port := conf.ListeningPort
 
-	// Rout
-	conf.Routes[0] = "/"
-
 	fmt.Printf("Listening on port: %s \n", port)
 	fmt.Printf("http://localhost:%s", port)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	_ = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
 
